50934230dfc82d4be5a32b81a05b8b876a56e61e
Arpik Sargsyan <arpiksargsyan@gmail.com>
2025-01-20 23:51:23 +0400

    Added the first excersize(for test)

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿// See https://aka.ms/new-console-template for more information
+/*
+ FizzBuzz excersize
+ */
+
+for (int i = 0; i < 15; i++)
+{
+    if (i % 3 == 0 && i % 5 == 0)
+    {
+        Console.WriteLine("FizzBuzz");
+    }
+    else if (i % 3 == 0)
+    {
+        Console.WriteLine("Buzz");
+    }
+    else if (i % 3 == 0)
+    {
+        Console.WriteLine("Fizz");
+    }
+    else
+    {
+        Console.WriteLine(i);
+    }
+}
